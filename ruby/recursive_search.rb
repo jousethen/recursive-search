95011f8fe34fcc96451700bf24f94980105618e9
jousethen <jthen93@gmail.com>
2021-12-01 16:37:05 -0500

    Done.

--- ruby/recursive_search.rb
+++ ruby/recursive_search.rb
@@ -1,5 +1,9 @@
 def recursive_search(arr, target)
   # type your code in here
+  return true if target == arr[0] # base case
+  return false if arr.length == 0
+
+  recursive_search(arr.slice(1,arr.length), target)
 end
 
 if __FILE__ == $PROGRAM_NAME
